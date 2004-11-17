999cb3b50305bd5ba10c7d980b4d4eec45db29ab
Rui Nuno Capela <rncbc@rncbc.org>
2004-11-17 13:05:42 +0000

    Deferred channel info update fixed again.
    git-svn-id: https://svn.linuxsampler.org/svn/qsampler/trunk@298 d2c4447f-3961-4fff-bec3-96d84be9abe5

--- src/qsamplerAbout.h
+++ src/qsamplerAbout.h
@@ -24,7 +24,7 @@
 
 #define QSAMPLER_TITLE        "qsampler"
 #define QSAMPLER_SUBTITLE     "A LinuxSampler Qt GUI Interface"
-#define QSAMPLER_VERSION      "0.0.3.11"
+#define QSAMPLER_VERSION      "0.0.3.12"
 #define QSAMPLER_WEBSITE      "http://qsampler.sourceforge.net"
 #define QSAMPLER_COPYRIGHT    "Copyright (C) 2004, rncbc aka Rui Nuno Capela. All rights reserved."
 
