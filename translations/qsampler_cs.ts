5b5dcb4434480049490e546a7655511e5fd39fbf
rncbc <rncbc@3e92087d-0f25-4bc2-b788-d05a45ecc5c1>
2009-10-06 17:43:58 +0000

    - lupdate translations.
    git-svn-id: http://svn.code.sf.net/p/qsampler/code_attic/trunk@324 3e92087d-0f25-4bc2-b788-d05a45ecc5c1

--- translations/qsampler_cs.ts
+++ translations/qsampler_cs.ts
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="cs_CZ">
+<!DOCTYPE TS><TS version="1.1" language="cs_CZ">
 <context>
     <name>MidiInstrumentsModel</name>
     <message>
