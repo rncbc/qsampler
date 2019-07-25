137058601ebfc197490381ab8ea2450f6f778ccc
rncbc <rncbc@rncbc.org>
2019-07-25 19:20:34 +0100

    - Removed double-colon (::) prefix from sigemptyset macro on MacOS.

--- src/qsamplerMainForm.cpp
+++ src/qsamplerMainForm.cpp
@@ -269,7 +269,7 @@ MainForm::MainForm ( QWidget *pParent )
 	// Install SIGTERM signal handler.
 	struct sigaction sigterm;
 	sigterm.sa_handler = qsampler_sigterm_handler;
-	::sigemptyset(&sigterm.sa_mask);
+	sigemptyset(&sigterm.sa_mask);
 	sigterm.sa_flags = 0;
 	sigterm.sa_flags |= SA_RESTART;
 	::sigaction(SIGTERM, &sigterm, NULL);
